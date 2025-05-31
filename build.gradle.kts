f9d52b660c6838a8c715485225bc622190a11421
adrian <adrian.kuhn@arcor.de>
2025-05-31 11:28:47 +0200

    chore(build): update project configurations and build scripts
    update Kotlin and Gradle settings, reorganize source sets, and configure JVM toolchain to improve project structure and compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,22 @@ repositories {
     mavenCentral()
 }
 
+sourceSets {
+    getByName("main"){
+        kotlin.srcDirs("src/main/kotlin")
+    }
+    getByName("test"){
+        kotlin.srcDirs("src/test/kotlin")
+    }
+}
+
 dependencies {
     testImplementation(kotlin("test"))
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(23)
 }
